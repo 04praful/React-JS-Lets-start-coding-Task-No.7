a4049a8cecce82c81321aa27e929264ba3177462
04praful <126102812+04praful@users.noreply.github.com>
2023-03-18 15:00:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Food Rs 10</div>
+      <div>Petrol Rs 100</div>
+      <div>Movies Rs 200</div>
+    </div>
+  );
+}
+
+export default App;
