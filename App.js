58a26266ba106972d904b5976dae9fbe81a9a678
04praful <126102812+04praful@users.noreply.github.com>
2023-03-18 15:06:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function ExpenseItem() {
+  return (
+    <div className="App">
+      <h3>Expense Items</h3>
+      <div>Food Rs 10</div>
+      <div>Petrol Rs 100</div>
+      <div>Movies Rs 200</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
